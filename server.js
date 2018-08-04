d7a20ff1a7cbb0edb3363133529dbf08a75d3cad
Daniel Salcedo <dsalcedo@outlook.com>
2018-08-04 17:00:50 -0500

    Changes

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const net = require('net');
 const request = require('request');
 const dotenv = require("dotenv").config();
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var public;
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+    
+    } else {
+        public =  iface.address;  
+    }
+    ++alias;
+  });
+});
+
 
 const host = dotenv.parsed.WEBHOOK;
 const port = dotenv.parsed.PORT;
@@ -33,4 +55,4 @@ const server = net.createServer(function(client) {
 
 });
 
-server.listen(port, 'localhost');
+server.listen(port, public);
